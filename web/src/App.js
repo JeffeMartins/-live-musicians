961138175f7f1a924504cd253b5fe9a1e8d82588
Leonardo Batista <leonardo.batista@revoluti.tech>
2020-02-20 17:19:14 -0300

    Atualização

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './global.css';
+import './SideBar.css';
 
 export default class App extends React.Component {
 
@@ -8,17 +9,33 @@ export default class App extends React.Component {
             <div id="app">
 
                 <aside>
-
                     <strong>Cadastrar</strong>
                     <form>
-                        <label htmlFor={github_username}>Usuário do Git Hub</label>
-                        <input name={github_username} id={github_username} required/>
+                        <div class="input-block">
+                            <label htmlFor={"github_username"}>Usuário do Git Hub</label>
+                            <input name={"github_username"} id={"github_username"} required/>
+                        </div>
 
-                        <label htmlFor={techs}>Tecnologias</label>
-                        <input name={techs} id={techs} required/>
+                        <div className="input-block">
+                            <label htmlFor={"techs"}>Tecnologias</label>
+                            <input name={"techs"} id={"techs"} required/>
+                        </div>
 
-                        <label htmlFor={github_username}>Usuário do Git Hub</label>
-                        <input name={github_username} id={github_username} required/>
+
+                        <div className="input-group">
+
+                            <div class="input-block">
+                                <label htmlFor={"latitude"}>Latitude</label>
+                                <input name={"latitude"} id={"latitude"} required/>
+                            </div>
+
+                            <div className="input-block">
+                                <label htmlFor={"logitude"}>Longitude</label>
+                                <input name={"logitude"} id={"logitude"} required/>
+                            </div>
+                        </div>
+
+                        <button type={"submit"}>Salvar</button>
                     </form>
                 </aside>
                 <main>
